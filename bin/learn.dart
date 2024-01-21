a082c7caadc195b8c60c732eb68c45680241bc94
sutooy <82921049+sutooy@users.noreply.github.com>
2024-01-21 16:36:29 +0700

    chore: learn list

--- bin/learn.dart
+++ bin/learn.dart
@@ -1,3 +1,8 @@
+// List adalah koleksi dari elemen, List dapat berupa integer & string,
+// tapi bila memasukan string kedalam integer (atau sebaliknya) maka akan muncul warn/error
+List<int> numbers = [1, 9, 2, 1, 7];
+List<String> names = ["budi", "Joko", 'Ludi', "Yanto"];
+
 void main() {
-  print("learn dart 2");
+  print("names : ${names.join(', ')} \nnumbers: ${numbers.join(', ')}");
 }
