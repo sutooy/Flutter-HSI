37f7d63d144872169b049c6e03ede0bfa0356323
sutooy <82921049+sutooy@users.noreply.github.com>
2024-02-21 20:39:34 +0700

    chore: add data

--- bin/learn.dart
+++ bin/learn.dart
@@ -1,8 +1,16 @@
 // List adalah koleksi dari elemen, List dapat berupa integer & string,
 // tapi bila memasukan string kedalam integer (atau sebaliknya) maka akan muncul warn/error
-List<int> numbers = [1, 9, 2, 1, 7];
-List<String> names = ["budi", "Joko", 'Ludi', "Yanto"];
+// List<int> numbers = [1, 9, 2, 1, 7];
+// List<String> names = ["budi", "Joko", 'Ludi', "Yanto"];
+// Map<String, int> daftarNilai = [10];
+// void main() {
+//   print("names : ${names.join(', ')} \nnumbers: ${numbers.join(', ')}");
+// }
 
-void main() {
-  print("names : ${names.join(', ')} \nnumbers: ${numbers.join(', ')}");
-}
+// void kaliSepuluh(int x) {
+//   print(10 * x);
+// }
+
+// void main() {
+//   kaliSepuluh(5);
+// }
